851774b77f91ec4f14839a95ac90517d6dc05e17
lukavyi-am <andlukaviy@gmail.com>
2020-08-14 16:37:56 +0300

    cxxcvcx140820

---
+++ src/net/threads/philosopher/ChopStick.java
@@ -0,0 +1,15 @@
+package net.threads.philosopher;
+
+public class ChopStick {
+    private boolean taken = false;
+    public synchronized void take() throws InterruptedException {
+        while (taken)
+            wait();
+        taken = true;
+    }
+
+    public synchronized void drop() {
+        taken = false;
+        notifyAll();
+    }
+}
